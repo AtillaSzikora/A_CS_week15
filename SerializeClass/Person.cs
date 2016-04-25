c3152635b76a58f6c5bb6c3bbd30bba17f6add98
AtillaSzikora <szikora.atilla@gmail.com>
2016-04-25 10:22:59 +0200

    Some basic serialization added and works

---
+++ SerializeClass/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace SerializeClass {
+
+	[Serializable]
+	internal class Person {
+
+        public Person(string firstName, string lastName, string birthDate) {
+            FirstName = firstName;
+            LastName = lastName;
+            DateOfBirth = birthDate; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string DateOfBirth { get; set; }
+
+		public void GetObjectData(SerializationInfo info, StreamingContext context) {
+			throw new NotImplementedException(); }
+	}
+}
---
+++ SerializeClass/SerializeClass.cs
@@ -0,0 +1,17 @@
+﻿// Write an application that uses XML serialization to serialize and deserialize the last class you created as part of your job.
+
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Soap;
+
+namespace SerializeClass {
+	class SerializeClass {
+
+		static void Main (string[] args) {
+			Person jozsef = new Person("Jozsi", "Joo", "2000.10.10");
+
+			FileStream fs = new FileStream("Person.Dat", FileMode.Create);
+			IFormatter fmt = new SoapFormatter();
+			fmt.Serialize(fs, jozsef); }
+	}
+}
